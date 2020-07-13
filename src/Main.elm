099a10e4e78d9a21c08fafcdb67f644e1aa46c36
Bill St. Clair <billstclair@gmail.com>
2020-07-12 23:00:18 -0400

    Remove scroll monitor. Clearing the cache worked on my iPhone.

--- src/Main.elm
+++ src/Main.elm
@@ -8049,8 +8049,9 @@ renderColumns model =
                 , style "right" <| (10 |> String.fromInt) ++ "px"
                 ]
                 [ floatingButtons model
-                , br
-                , text (model.bodyScroll.scrollLeft |> String.fromFloat)
+
+                --, br
+                --, text (model.bodyScroll.scrollLeft |> String.fromFloat)
                 ]
         , table
             [ style "border-spacing" <| String.fromInt columnsBorderSpacing
