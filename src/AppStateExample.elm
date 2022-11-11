72fd74339bf6e58d3b10c0ce0c56e563e23a2b3d
Bill St. Clair <billstclair@gmail.com>
2022-11-11 13:40:20 -0500

    Finish UI for AppStateExample

--- src/AppStateExample.elm
+++ src/AppStateExample.elm
@@ -16,6 +16,7 @@ import Browser
 import Dict exposing (Dict)
 import DynamoDB
 import DynamoDB.AppState as AppState exposing (AppState, Error, InitialLoad)
+import DynamoDB.Html exposing (renderTable)
 import DynamoDB.Types as Types
     exposing
         ( Account
@@ -30,14 +31,20 @@ import Html
         ( Attribute
         , Html
         , b
+        , button
         , div
         , h2
+        , input
         , option
         , p
         , select
+        , table
+        , td
         , text
+        , th
+        , tr
         )
-import Html.Attributes exposing (selected, style, value)
+import Html.Attributes exposing (class, selected, size, style, value)
 import Html.Events exposing (on, onClick, onInput, targetValue)
 import Json.Decode as JD exposing (Decoder)
 import Json.Encode as JE exposing (Value)
@@ -55,16 +62,29 @@ main =
 
 
 type alias Model =
-    { account : Account
-    , appState : AppState
-    , display : String
+    { display : String
     , accounts : List Account
+    , account : Account
+    , appState : AppState
+    , columns : List String
+    , row : Row
+    , selection : Row
+    , rows : List Row
+    , columnName : String
     }
 
 
 type Msg
     = ReceiveAccounts (Result Error (List Account))
     | SetAccount String
+    | SetSelection Row
+    | SetColumnName String
+    | AddColumn
+    | RemoveColumn
+    | SetRowColumn String String
+    | AddRow
+    | RemoveRow
+    | UpdateRow
 
 
 emptyAccount : Account
@@ -84,12 +104,24 @@ emptyAppState =
     AppState.makeAppState emptyAccount
 
 
+type alias Row =
+    Dict String String
+
+
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { account = emptyAccount
-      , appState = emptyAppState
-      , display = "Fetching accounts..."
+    ( { display = "Fetching accounts..."
       , accounts = []
+      , account = emptyAccount
+      , appState = emptyAppState
+      , columns = [ "key", "value" ]
+      , row = Dict.empty
+      , selection = Dict.empty
+      , rows =
+            [ Dict.fromList [ ( "key", "key1" ), ( "value", "value1" ) ]
+            , Dict.fromList [ ( "key", "key2" ), ( "value", "value2" ) ]
+            ]
+      , columnName = "key"
       }
     , Task.attempt ReceiveAccounts
         (DynamoDB.readAccounts <| Just "accounts.json")
@@ -154,6 +186,98 @@ update msg model =
                     , Cmd.none
                     )
 
+        SetSelection row ->
+            ( { model
+                | selection = row
+                , row = row
+              }
+            , Cmd.none
+            )
+
+        SetColumnName name ->
+            ( { model | columnName = name }
+            , Cmd.none
+            )
+
+        AddColumn ->
+            if
+                (model.columnName == "*")
+                    || List.member model.columnName model.columns
+            then
+                ( model, Cmd.none )
+
+            else
+                ( { model
+                    | columns =
+                        List.append model.columns [ model.columnName ]
+                  }
+                , Cmd.none
+                )
+
+        RemoveColumn ->
+            if model.columnName == "key" then
+                ( model, Cmd.none )
+
+            else
+                ( { model
+                    | columns =
+                        LE.filterNot ((==) model.columnName) model.columns
+                  }
+                , Cmd.none
+                )
+
+        SetRowColumn columnName v ->
+            let
+                row =
+                    if v == "" then
+                        Dict.remove columnName model.row
+
+                    else
+                        Dict.insert columnName v model.row
+            in
+            ( { model | row = row }
+            , Cmd.none
+            )
+
+        AddRow ->
+            ( { model
+                | selection = model.row
+                , rows =
+                    model.rows ++ [ model.row ]
+              }
+            , Cmd.none
+            )
+
+        RemoveRow ->
+            ( { model
+                | selection =
+                    if model.row == model.selection then
+                        Dict.empty
+
+                    else
+                        model.selection
+                , rows = LE.filterNot ((==) model.row) model.rows
+              }
+            , Cmd.none
+            )
+
+        UpdateRow ->
+            ( { model
+                | selection = model.row
+                , rows =
+                    List.map
+                        (\row ->
+                            if row == model.selection then
+                                model.row
+
+                            else
+                                row
+                        )
+                        model.rows
+              }
+            , Cmd.none
+            )
+
 
 b : String -> Html msg
 b string =
@@ -165,9 +289,47 @@ br =
     Html.br [] []
 
 
+rowTable : Row -> Model -> Html Msg
+rowTable row model =
+    let
+        columnTh columnName =
+            th [] [ text columnName ]
+
+        columnTd columnName =
+            let
+                v =
+                    case Dict.get columnName model.row of
+                        Nothing ->
+                            ""
+
+                        Just s ->
+                            s
+            in
+            td []
+                [ input
+                    [ value v
+                    , size 20
+                    , onInput <| SetRowColumn columnName
+                    ]
+                    []
+                ]
+    in
+    table [ class "prettytable" ] <|
+        tr [] (List.map columnTh model.columns)
+            :: List.map columnTd model.columns
+
+
 view : Model -> Html Msg
 view model =
-    div []
+    let
+        addSelectionMarker row =
+            if row == model.selection then
+                Dict.insert "*" "*" row
+
+            else
+                row
+    in
+    div [ style "margin" "8px" ]
         [ h2 []
             [ text "AppState Example" ]
         , p [ style "color" "red" ]
@@ -179,7 +341,50 @@ view model =
             , b "TableName: "
             , text model.account.tableName
             ]
+        , p []
+            [ b "Column name: "
+            , input
+                [ size 20
+                , onInput SetColumnName
+                , value model.columnName
+                ]
+                []
+            , br
+            , button [ onClick AddColumn ]
+                [ text "Add column" ]
+            , text " "
+            , button [ onClick RemoveColumn ]
+                [ text "Remove column" ]
+            ]
+        , p []
+            [ rowTable model.row model
+            , button [ onClick AddRow ]
+                [ text "Add" ]
+            , text " "
+            , button [ onClick RemoveRow ]
+                [ text "Remove" ]
+            , text " "
+            , button [ onClick UpdateRow ]
+                [ text "Update" ]
+            ]
+        , p []
+            [ renderTable SetSelection
+                { tableConfig | columnDescriptors = "*" :: model.columns }
+                (List.map addSelectionMarker model.rows)
+            ]
         ]
+
+
+tableConfig =
+    { columnDescriptors = []
+    , columnDescriptorToString = identity
+    , elementToString = getRowValue
+    }
+
+
+getRowValue : String -> Row -> Maybe String
+getRowValue column row =
+    Dict.get column row
 
 
 accountSelector : Model -> Html Msg
